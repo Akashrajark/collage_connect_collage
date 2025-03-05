e6678d318c10948e60a5135a5b38f07e5ebe3411
jishnu6241 <jishnuov2002@gmail.com>
2025-03-06 02:07:44 +0530

    some more changes

--- lib/features/login/login_screen.dart
+++ lib/features/login/login_screen.dart
@@ -110,12 +110,15 @@
 //   }
 // }
 
+import 'package:collage_connect_collage/common_widget/custom_alert_dialog.dart';
 import 'package:collage_connect_collage/common_widget/custom_button.dart';
 import 'package:collage_connect_collage/common_widget/custom_text_formfield.dart';
 import 'package:collage_connect_collage/features/home/home_screen.dart';
+import 'package:collage_connect_collage/features/login/login_bloc/login_bloc.dart';
 import 'package:collage_connect_collage/theme/app_theme.dart';
 import 'package:collage_connect_collage/util/value_validator.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class Loginscreen extends StatefulWidget {
   const Loginscreen({super.key});
@@ -134,111 +137,144 @@ class _LoginScreenState extends State<Loginscreen> {
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: Colors.white,
-        body: Stack(
-          children: [
-            Image.network(
-              'https://images.unsplash.com/photo-1568792923760-d70635a89fdc?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-              fit: BoxFit.cover,
-              width: double.infinity,
-            ),
-            Center(
-              child: Container(
-                width: 350,
-                decoration: BoxDecoration(
-                  color: Colors.black.withAlpha(50),
-                  border: Border.all(color: primaryColor, width: 2),
-                  borderRadius: BorderRadius.circular(20),
-                ),
-                child: Padding(
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      const SizedBox(
-                        height: 30,
-                      ),
-                      Text(
-                        'Log in - Collage',
-                        style:
-                            Theme.of(context).textTheme.headlineLarge!.copyWith(
-                                  color: Colors.white.withAlpha(220),
-                                  fontWeight: FontWeight.bold,
-                                ),
-                        textAlign: TextAlign.left,
-                      ),
-                      const SizedBox(
-                        height: 20,
-                      ),
-                      Text(
-                        'Email',
-                        style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-                              color: Colors.white70,
-                              fontWeight: FontWeight.bold,
-                            ),
-                        textAlign: TextAlign.left,
-                      ),
-                      const SizedBox(
-                        height: 5,
-                      ),
-                      CustomTextFormField(
-                          labelText: 'Enter email',
-                          controller: _emailController,
-                          validator: emailValidator,
-                          isLoading: false),
-                      const SizedBox(
-                        height: 15,
-                      ),
-                      Text(
-                        'Password',
-                        style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-                              color: Colors.white70,
-                              fontWeight: FontWeight.bold,
-                            ),
-                        textAlign: TextAlign.left,
-                      ),
-                      const SizedBox(
-                        height: 5,
-                      ),
-                      TextFormField(
-                          controller: _passwordController,
-                          obscureText: isObscure,
-                          decoration: InputDecoration(
-                            filled: true,
-                            suffixIcon: IconButton(
-                                onPressed: () {
-                                  isObscure = !isObscure;
-                                  setState(() {});
-                                },
-                                icon: Icon(isObscure
-                                    ? Icons.visibility_off
-                                    : Icons.visibility)),
-                            border: const OutlineInputBorder(),
-                            labelText: 'Password',
-                            prefixIcon: const Icon(Icons.lock),
-                          )),
-                      const SizedBox(
-                        height: 30,
-                      ),
-                      CustomButton(
-                        inverse: true,
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => const HomeScreen(),
-                            ),
-                          );
-                        },
-                        label: 'Login',
-                      )
-                    ],
+        body: BlocProvider(
+          create: (context) => LoginBloc(),
+          child: BlocConsumer<LoginBloc, LoginState>(
+            listener: (context, state) {
+              if (state is LoginSuccessState) {
+                Navigator.pushAndRemoveUntil(
+                  context,
+                  MaterialPageRoute(builder: (context) => const HomeScreen()),
+                  (route) => false,
+                );
+              } else if (state is LoginFailureState) {
+                showDialog(
+                  context: context,
+                  builder: (context) => CustomAlertDialog(
+                    title: 'Failed',
+                    description: state.message,
+                    primaryButton: 'Ok',
                   ),
-                ),
-              ),
-            ),
-          ],
+                );
+              }
+            },
+            builder: (context, state) {
+              return Stack(
+                children: [
+                  Image.network(
+                    'https://images.unsplash.com/photo-1568792923760-d70635a89fdc?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+                    fit: BoxFit.cover,
+                    width: double.infinity,
+                  ),
+                  Center(
+                    child: Container(
+                      width: 350,
+                      decoration: BoxDecoration(
+                        color: Colors.black.withAlpha(50),
+                        border: Border.all(color: primaryColor, width: 2),
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 20, vertical: 20),
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const SizedBox(
+                              height: 30,
+                            ),
+                            Text(
+                              'Log in - Collage',
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .headlineLarge!
+                                  .copyWith(
+                                    color: Colors.white.withAlpha(220),
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                              textAlign: TextAlign.left,
+                            ),
+                            const SizedBox(
+                              height: 20,
+                            ),
+                            Text(
+                              'Email',
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .bodyMedium!
+                                  .copyWith(
+                                    color: Colors.white70,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                              textAlign: TextAlign.left,
+                            ),
+                            const SizedBox(
+                              height: 5,
+                            ),
+                            CustomTextFormField(
+                                labelText: 'Enter email',
+                                controller: _emailController,
+                                validator: emailValidator,
+                                isLoading: false),
+                            const SizedBox(
+                              height: 15,
+                            ),
+                            Text(
+                              'Password',
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .bodyMedium!
+                                  .copyWith(
+                                    color: Colors.white70,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                              textAlign: TextAlign.left,
+                            ),
+                            const SizedBox(
+                              height: 5,
+                            ),
+                            TextFormField(
+                                controller: _passwordController,
+                                obscureText: isObscure,
+                                decoration: InputDecoration(
+                                  filled: true,
+                                  suffixIcon: IconButton(
+                                      onPressed: () {
+                                        isObscure = !isObscure;
+                                        setState(() {});
+                                      },
+                                      icon: Icon(isObscure
+                                          ? Icons.visibility_off
+                                          : Icons.visibility)),
+                                  border: const OutlineInputBorder(),
+                                  labelText: 'Password',
+                                  prefixIcon: const Icon(Icons.lock),
+                                )),
+                            const SizedBox(
+                              height: 30,
+                            ),
+                            CustomButton(
+                              inverse: true,
+                              onPressed: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => const HomeScreen(),
+                                  ),
+                                );
+                              },
+                              label: 'Login',
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              );
+            },
+          ),
         ));
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:collage_connect_collage/features/login/login_screen.dart';
 import 'package:collage_connect_collage/theme/app_theme.dart';
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Supabase.initialize(
+      anonKey:
+          'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNnaXhtaXhnamhzcHVxYXZxanN1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzI5NDMwMjgsImV4cCI6MjA0ODUxOTAyOH0.gb43EJ1vmms_HHfdvj9bZWmAQ2Fv_qMm_yoSsjHBE8s',
+      url: 'https://sgixmixgjhspuqavqjsu.supabase.co');
+
   runApp(const MyApp());
 }
 
